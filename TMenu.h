190490c74faf89c0d72a9a2b510752df0fa0b136
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-10-30 14:08:47 +0100

    Klasa abstrakcyjna

--- TMenu.h
+++ TMenu.h
@@ -1,5 +1,24 @@
 #pragma once
-class TMenu
+/*Zadanie: przygotowaæ klasê abstrakcyjn¹ (=interface) 
+dla TMenu.
+Przypomnienie: Klasa abstrakcyjna to klasa która nie mo¿e mieæ instancji */
+class TMenuAbs {  //to jest klasa abstrakcyjna (bo zawiera 
+	//konstrukcje postaci: virtual ... = 0;
+public: 
+	virtual void addAll(int ilePar, ...) = 0; 
+	virtual unsigned int wybierz() = 0;
+	TMenuAbs() {};
+	virtual ~TMenuAbs() {};
+	/*
+	  implementacja tego interfejsu powinna funkcjonowaæ tak:
+    	TMenu* mnu = new TMenu();
+		mnu->addAll(2, "Opcja 1", "Opcja 2"); 
+		  (automatyczna numeracja + 0. Wyjscie).
+		auto opcja = mnu->wybierz();
+		delete mnu;
+	*/
+};
+class TMenu: public TMenuAbs
 {
 private:
 	vector<string> tabItemsV;
